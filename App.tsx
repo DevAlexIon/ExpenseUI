f4ad57e75ca018cadd0e982cea6d68d2268ebc8f
Alexandru Ion <143385053+AlexandruIon00@users.noreply.github.com>
2024-02-29 14:30:48 +0200

    -feat: build the first swiper screen.

--- App.tsx
+++ App.tsx
@@ -1,117 +1,124 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
+  Dimensions,
+  StyleSheet,
   View,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
+import {SwiperFlatList} from 'react-native-swiper-flatlist';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+const App = () => (
+  <View style={styles.container}>
+    <SwiperFlatList
+      index={0}
+      paginationStyle={styles.paginationStyle}
+      paginationStyleItemActive={styles.paginationStyleItemActive}
+      paginationStyleItemInactive={styles.paginationStyleItemInactive}
+      showPagination>
+      <View style={[styles.child]}>
+        <Image source={require('./assets/Illustrationfirst.jpg')} />
+        <Text style={styles.title}>Gain total control of your money</Text>
+        <Text style={styles.subtitle}>
+          Become your own money manager and make every cent count
+        </Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => console.log('Sign up pressed')}>
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={() => console.log('Sign up pressed')}>
+            <Text style={styles.loginButtonText}>Login</Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+      </View>
+      {/* <View style={[styles.child, {backgroundColor: 'thistle'}]}>
+        <Text>2</Text>
+      </View>
+      <View style={[styles.child, {backgroundColor: 'skyblue'}]}>
+        <Text>3</Text>
+      </View> */}
+    </SwiperFlatList>
+  </View>
+);
+
+const {width} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
+  container: {flex: 1, backgroundColor: 'white', marginTop: 50},
+  child: {width, justifyContent: 'center', alignItems: 'center'},
+  title: {
+    width: 277,
+    fontFamily: 'Inter',
     fontWeight: '700',
+    fontSize: 32,
+    lineHeight: 39,
+    textAlign: 'center',
+    marginTop: 41,
+    color: '#212325',
+  },
+  subtitle: {
+    width: 272,
+    fontWeight: '500',
+    fontFamily: 'Inter',
+    fontSize: 16,
+    lineHeight: 19.36,
+    color: '#91919F',
+    textAlign: 'center',
+    marginTop: 17,
+  },
+  button: {
+    backgroundColor: '#7F3DFF',
+    borderRadius: 8,
+    paddingVertical: 17,
+    paddingHorizontal: 137.5,
+    alignSelf: 'center',
+  },
+  loginButton: {
+    backgroundColor: '#EEE5FF',
+    borderRadius: 8,
+    paddingVertical: 17,
+    paddingHorizontal: 147,
+    marginTop: 20,
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: '#FCFCFC',
+    fontSize: 18,
+    fontWeight: '600',
+    textAlign: 'center',
+    fontFamily: 'Inter',
+    lineHeight: 22,
+  },
+  loginButtonText: {
+    color: '#7F3DFF',
+    fontSize: 18,
+    fontWeight: '600',
+    textAlign: 'center',
+    fontFamily: 'Inter',
+    lineHeight: 22,
+  },
+  buttonContainer: {
+    paddingTop: 100,
+  },
+  paginationStyle: {
+    position: 'absolute',
+    bottom: 230,
+    alignItems: 'center',
+  },
+  paginationStyleItemActive: {
+    backgroundColor: '#7F3DFF',
+    width: 16,
+    height: 16,
+  },
+  paginationStyleItemInactive: {
+    backgroundColor: '#EEE5FF',
+    width: 8,
+    height: 8,
   },
 });
 
